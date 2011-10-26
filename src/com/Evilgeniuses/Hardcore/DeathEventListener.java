489e0c57db82f07952e98058b063e7e109d2a2c8
Allen Lyons <allen.lyons@gmail.com>
2011-10-26 14:17:46 -0400

    Thunder and lightning, first attempt. There is fire, but I think it might only be client side fire

--- src/com/Evilgeniuses/Hardcore/DeathEventListener.java
+++ src/com/Evilgeniuses/Hardcore/DeathEventListener.java
@@ -1,5 +1,7 @@
 package com.Evilgeniuses.Hardcore;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityListener;
@@ -22,8 +24,16 @@ public class DeathEventListener extends EntityListener {
 		PlayerDeathEvent de = (PlayerDeathEvent)event;
 				
 		_plugin.getDeadPlayerList().addPlayer(player,de.getDeathMessage());
+				
+		if (_plugin.getHardcoreConfiguration().doThunderAndLightningOnDeath)
+			doSoundAndFury(player.getLocation(),player.getWorld());
 		
-		
+	}
+	
+	public void doSoundAndFury(Location where, World whatWorld) {
+		whatWorld.strikeLightningEffect(where);
+		whatWorld.setThunderDuration(_plugin.getHardcoreConfiguration().thunderLengthSeconds*20);
+		whatWorld.setThundering(true);
 	}
 	
 }
--- src/com/Evilgeniuses/Hardcore/HardcoreConfiguration.java
+++ src/com/Evilgeniuses/Hardcore/HardcoreConfiguration.java
@@ -7,15 +7,21 @@ public class HardcoreConfiguration {
 	//one week
 	private int DEFAULT_DEATH_DURATION_SECONDS = 7 * 24 * 60 * 60;
 	private int DEFAULT_REAPER_CHECK_SECONDS = 10;
+	private boolean DEFAULT_THUNDER_LIGHTNING = true;
+	private int DEFAULT_THUNDER_LENGTH_SECONDS = 1;
 		
 	public int deathSeconds;
 	public int reaperCheckSeconds;
+	public boolean doThunderAndLightningOnDeath;
+	public int thunderLengthSeconds;
 
 	HardcoreConfiguration(HardcorePlugin plugin) {
 		Configuration config = plugin.getConfiguration();
 		
 		this.deathSeconds=config.getInt("deathSeconds", DEFAULT_DEATH_DURATION_SECONDS);
 		this.reaperCheckSeconds=config.getInt("reaperCheckSeconds", DEFAULT_REAPER_CHECK_SECONDS);
+		this.doThunderAndLightningOnDeath=config.getBoolean("doThunderAndLightningOnDeath", DEFAULT_THUNDER_LIGHTNING);
+		this.thunderLengthSeconds=config.getInt("thunderLengthSeconds",DEFAULT_THUNDER_LENGTH_SECONDS);
 		
 		config.save();	
 	}
